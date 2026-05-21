8db871ddbfccb408a86b061b09b5b4c6c978d81f
poorna083 <dpoorna083@gmail.com>
2026-05-21 22:22:59 +0530

    inserted

--- arrays/maxconones.py
+++ arrays/maxconones.py
@@ -1,7 +1,19 @@
 def maxconsicativeones(nums):
-    
+    max_once = 0
+    count = 0
+    for i in nums:
+        if i == 1:
+            count += 1
+        else:
+            if count > max_once:
+                max_once = count
+            count = 0
+    if count > max_once:
+        max_once = count
+
+    return max_once
 
 
-
-nums=eval(input("Enter the list :"))
-maxconsicativeones(nums)
+nums = eval(input("Enter the list : "))
+res = maxconsicativeones(nums)
+print(res)
